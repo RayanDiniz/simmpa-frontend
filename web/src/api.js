94a16b02ca1ea7351619677362e7febf44993f5f
RAYAN CASSIO VIEIRA DINIZ <55702163+RayanDiniz@users.noreply.github.com>
2026-03-05 18:17:44 +0000

    api

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://seu-app-no-render.onrender.com'
+  baseURL: 'https://simmpa-backend.onrender.com/'
 });
 
 export default api;
